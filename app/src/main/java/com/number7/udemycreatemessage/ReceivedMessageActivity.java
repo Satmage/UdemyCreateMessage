ced43192f8473f376f347d71d97823d8cfa2e77f
Andrei Ivanou <Kleckers@yandex.ru>
2019-01-31 14:40:32 +0300

    add class ReceivedMessageActivity.java

---
+++ app/src/main/java/com/number7/udemycreatemessage/ReceivedMessageActivity.java
@@ -0,0 +1,13 @@
+package com.number7.udemycreatemessage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceivedMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recieved_message);
+    }
+}
