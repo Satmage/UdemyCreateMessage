685a42bd96ef971744aced831dd94a8c4f105fe5
Andrei Ivanou <Kleckers@yandex.ru>
2019-02-01 15:04:47 +0300

    update button handler in MainActivity.java - onClickSendMessage();

--- app/src/main/java/com/number7/udemycreatemessage/MainActivity.java
+++ app/src/main/java/com/number7/udemycreatemessage/MainActivity.java
@@ -1,17 +1,28 @@
 package com.number7.udemycreatemessage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextMessage = findViewById(R.id.editTextMessage);
     }
 
     public void onClickSendMessage(View view) {
+        String message = editTextMessage.getText().toString();
+        Intent intent = new Intent(this, ReceivedMessageActivity.class);
+        intent.putExtra("message", message);
+        startActivity(intent);
     }
 }
