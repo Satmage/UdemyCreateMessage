2641512bd1f5d0de28f44da31fea176198a4a9db
Andrei Ivanou <Kleckers@yandex.ru>
2019-02-01 15:11:23 +0300

    update ReceivedMessageActivity.java - onCreate();

--- app/src/main/java/com/number7/udemycreatemessage/ReceivedMessageActivity.java
+++ app/src/main/java/com/number7/udemycreatemessage/ReceivedMessageActivity.java
@@ -1,13 +1,23 @@
 package com.number7.udemycreatemessage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceivedMessageActivity extends AppCompatActivity {
+
+    private TextView textViewReceivedMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recieved_message);
+
+        textViewReceivedMessage = findViewById(R.id.textViewReceivedMessage);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        textViewReceivedMessage.setText(message);
     }
 }
